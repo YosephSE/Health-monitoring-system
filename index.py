057823fc8a5eae5353ceeb38983a9538188191b1
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-12 20:56:58 +0300

    port added

--- index.py
+++ index.py
@@ -448,4 +448,5 @@ def signout():
     return redirect(url_for('home'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)
